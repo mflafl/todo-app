d27597108a03475b6ff12b77e593ece5c3ce7550
mflafl <mflafl>
2015-07-17 00:09:21 +0300

    computed filter

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,21 +6,15 @@ default Ember.Controller.extend({
   search: null,
   category: null,
   tag: null,
+  filteredItems: Ember.computed.filter('items', function(item) {
+    var category = this.get('category');
+    //console.log(item.category.name);
+    return true;
+  }),
   items: function() {
     var items = this.get('model').items;
-    /*var tag = this.get('tag');
-    var category = this.get('category');
-    
-    if (tag) {
-      return items.filterBy('tag', tag);
-    } else if (category) {
-      return items.filterBy('category', category);
-    } else {
-      //return items.filterBy('name', 'Untitled22');
-    }*/
-    console.log(this.get('model').items);
-    return items.filterBy('done', false);
-  }.property('model'),  
+    return items;
+  }.property('model'),
   tags: function() {
     return this.get('model').tags;
   }.property('model'),
